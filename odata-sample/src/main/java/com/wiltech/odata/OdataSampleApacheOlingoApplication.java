182f2f26b051d65e7dce98853b21138a179a0735
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-06-10 12:14:06 +0100

    Done

--- odata-sample/src/main/java/com/wiltech/odata/OdataSampleApacheOlingoApplication.java
+++ odata-sample/src/main/java/com/wiltech/odata/OdataSampleApacheOlingoApplication.java
@@ -2,11 +2,20 @@ package com.wiltech.odata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * The type Odata sample apache olingo application.
+ */
 @SpringBootApplication
+@ServletComponentScan
 public class OdataSampleApacheOlingoApplication {
 
-	public static void main(String[] args) {
+    /**
+     * The entry point of application.
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
 		SpringApplication.run(OdataSampleApacheOlingoApplication.class, args);
 	}
 
---
+++ odata-sample/src/main/java/com/wiltech/odata/config/ServletConfigBean.java
@@ -0,0 +1,17 @@
+/*
+ * (c) Midland Software Limited 2019
+ * Name     : ServletConfigBean.java
+ * Author   : ferraciolliw
+ * Date     : 10 Jun 2019
+ */
+package com.wiltech.odata.config;
+
+import org.springframework.context.annotation.Bean;
+
+/**
+ *
+ */
+public class ServletConfigBean {
+
+
+}
