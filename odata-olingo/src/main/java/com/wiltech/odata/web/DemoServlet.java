a87305919c4a6d6abf30f83238ad4f3bdf2ed337
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-06-11 08:26:53 +0100

    Finished lesson 1 get a set of products

---
+++ odata-olingo/src/main/java/com/wiltech/odata/web/DemoServlet.java
@@ -0,0 +1,57 @@
+/*
+ * (c) Midland Software Limited 2019
+ * Name     : DemoServlet.java
+ * Author   : ferraciolliw
+ * Date     : 10 Jun 2019
+ */
+package com.wiltech.odata.web;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.olingo.commons.api.edmx.EdmxReference;
+import org.apache.olingo.server.api.OData;
+import org.apache.olingo.server.api.ODataHttpHandler;
+import org.apache.olingo.server.api.ServiceMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.wiltech.odata.service.DemoEdmProvider;
+import com.wiltech.odata.service.DemoEntityCollectionProcessor;
+
+/**
+ * The type Demo servlet.
+ */
+public class DemoServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOG = LoggerFactory.getLogger(DemoServlet.class);
+
+    /**
+     * Service.
+     * @param req the req
+     * @param resp the resp
+     * @throws ServletException the servlet exception
+     * @throws IOException the io exception
+     */
+    protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            // create odata handler and configure it with CsdlEdmProvider and Processor
+            OData odata = OData.newInstance();
+            ServiceMetadata edm = odata.createServiceMetadata(new DemoEdmProvider(), new ArrayList<EdmxReference>());
+            ODataHttpHandler handler = odata.createHandler(edm);
+            handler.register(new DemoEntityCollectionProcessor());
+
+            // let the handler do the work
+            handler.process(req, resp);
+        } catch (RuntimeException e) {
+            LOG.error("Server Error occurred in ExampleServlet", e);
+            throw new ServletException(e);
+        }
+    }
+}
