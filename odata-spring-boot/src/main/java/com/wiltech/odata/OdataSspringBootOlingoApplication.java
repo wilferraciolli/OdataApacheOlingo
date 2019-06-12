ef51ab646cbe8b94691e57aafafb02d49de0929b
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-06-12 11:16:09 +0100

    Added olingo odata 2 spring with jersey

---
+++ odata-spring-boot/src/main/java/com/wiltech/odata/OdataSspringBootOlingoApplication.java
@@ -0,0 +1,13 @@
+package com.wiltech.odata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdataSspringBootOlingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdataSspringBootOlingoApplication.class, args);
+	}
+
+}
---
+++ odata-spring-boot/src/test/java/com/wiltech/odata/OdataSspringBootOlingoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wiltech.odata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OdataSspringBootOlingoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
